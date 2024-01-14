2b7c9ab9bd332203a56028e44749bfa58609ff8b
pedrothomaz75 <pedroimachida@gmail.com>
2024-01-14 00:50:59 -0300

    initial commit

---
+++ secao-1/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * HelloWorld = Nome da minha classe
+ * public = Tipo de acesso da classe
+ * class = Tipo da classe
+ */
+public class HelloWorld {
+
+   // Todo o conteudo deverá ser inserido aqui dentro 
+
+}
+// Fora do escopo da classe
