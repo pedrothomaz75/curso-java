67e8ea0e1aa0496ab8653cafaf87d8f79a865ff7
pedrothomaz75 <pedroimachida@gmail.com>
2024-01-14 00:59:49 -0300

    feat: learning about psvm

--- secao-1/HelloWorld.java
+++ secao-1/HelloWorld.java
@@ -4,6 +4,19 @@
  * class = Tipo da classe
  */
 public class HelloWorld {
+    // Método executor das classes no Java
+    
+    /*
+     * public = modificador de acesso, metodo pode ser acessado de qualquer parte do programa
+     * static = indica que metodo main pertence a classe HelloWorld e não a uma instancia dela
+     * void = tipo de retorno que o metodo vai ter
+     * main = nome do método, precisa obrigatoriamente ser main
+     * string = tipo do conteudo de argumentos que vai ser passado
+     * [] = lista de argumentos 
+     */
+    public static void main(String[] args) {
+        
+    }
 
    // Todo o conteudo deverá ser inserido aqui dentro 
 
