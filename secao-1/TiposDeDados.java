83c264f2593130faf207bcd90843ca59e10deb65
pedrothomaz75 <pedroimachida@gmail.com>
2024-01-14 01:13:03 -0300

    feat: "primitive types"

---
+++ secao-1/TiposDeDados.java
@@ -0,0 +1,20 @@
+public class TiposDeDados {
+    public static void main(String[] args) {
+        // Valores (int, double, float, long)
+        // Textos (String)
+        // Boleano (boolean)
+
+        // tipo_primitivo nomeDaVariavel = valor;
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f; // Necessita do "f" do nome da variável
+        long dadoDoTipoLong = 4849455555465465465L; // Precisa do "L" e uma range específica
+        String dadoDoSTipotring = "Qualquer texto que eu quiser";
+        boolean dadoDoTipoBoolean = false; // true or false
+
+        // FLOAT x DOUBLE
+        //  Float tem uma precisão a mais do que o double com relação a números quebrados
+
+
+    }
+}
