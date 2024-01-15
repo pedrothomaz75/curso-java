dc1b7a3f321985bf89540efe3fcc0625727e777d
pedrothomaz75 <pedroimachida@gmail.com>
2024-01-14 22:47:39 -0300

    feat: adding condition structure

---
+++ secao-1/EstruturaCondicao.java
@@ -0,0 +1,16 @@
+public class EstruturaCondicao {
+    public static void main(String[] args) {
+        // Declarando...
+
+        int numero = 10;
+        if(numero == 10) {
+            // sysout = método de printar coisas na tela
+            System.out.println("Entrou no if");
+        } else if(numero == 0) {
+            System.out.println("Entrou no else if");
+        } else {
+            System.out.println("Entrou no else");
+        }
+
+    }
+}
